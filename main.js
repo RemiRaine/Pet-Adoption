171f1ca7f02ea4696bbdb2fb7d3c5b926694d886
RANDOLPH O. QUILATON <HMO-002@spcmc.com>
2024-05-24 14:58:28 +0800

    Finished pet pet filtering

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ async function start() {
 
 start()
 
-async function petsArea () {
+async function petsArea() {
     const petsPromise =  await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
